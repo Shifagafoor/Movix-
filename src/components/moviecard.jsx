d2ed647f57a55a1ae916dd7cdc3bc48a201a8259
Shifa gafoor <shifagafoorncas@gmail.com>
2026-07-17 12:02:14 +0530

    .

--- src/components/moviecard.jsx
+++ src/components/moviecard.jsx
@@ -1,11 +1,13 @@
 function moviecard(){
     return(
         <div className = "movie-card">
+            img
             <div className="movies-things">
                 <h2>Movie Title</h2>
                 <p>⭐8.5</p>
             </div>
         </div>
+        
     );
 }
 export default moviecard;
