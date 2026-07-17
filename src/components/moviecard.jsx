cd38230c24a3ce07734628726603bb10f6f15497
Shifa gafoor <shifagafoorncas@gmail.com>
2026-07-17 12:26:12 +0530

    .

--- src/components/moviecard.jsx
+++ src/components/moviecard.jsx
@@ -1,7 +1,10 @@
+import "../styles/moviecard.css";
+import vazha from "../assets/vazha 2.png";
+
 function moviecard(){
     return(
         <div className = "movie-card">
-            img
+            <img src= {vazha} alt="Movie Poster" className="movie-post"/>
             <div className="movies-things">
                 <h2>Movie Title</h2>
                 <p>⭐8.5</p>
