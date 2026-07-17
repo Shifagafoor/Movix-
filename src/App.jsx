f0e6bfbe1b580204408e89876d9a4a8f20e9cab8
Shifa gafoor <shifagafoorncas@gmail.com>
2026-07-17 10:17:01 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/navbar";
 import SearchBar from "./components/SearchBar";
+import Hero from "./components/hero";
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
       <h1>Movix</h1>
       <p>Discover your Favorite Movies</p>
       <SearchBar/>
---
+++ src/components/hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+    return (
+        <div className="hero">
+            <h1>Welcome to Our Movie App</h1>
+            <p>Find your favorite movies instantly.</p>
+        </div>
+    );
+}
+
+export default Hero;
