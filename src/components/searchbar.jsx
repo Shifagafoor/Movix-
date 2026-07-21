b2bb04f60021fd3782f3f29cec259fdb71d29707
Shifa gafoor <shifagafoorncas@gmail.com>
2026-07-21 16:31:06 +0530

    .

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,10 +1,21 @@
+import {useState} from "react";
 import "../styles/searchbar.css";
 
 function SearchBar() {
+    const [searchText, setSearchText] = useState("");
+    
+    function handleSearch() {
+        alert(searchText);
+    }
     return(
         <div className = "search">
-            <input className="search-input" type="text" placeholder="Search for a movie" />
-            <button>search</button>
+            <input className="search-input" type="text" 
+            placeholder="Search for a movie" 
+            value={searchText} 
+            onChange={(e) => setSearchText(e.target.value)}/>
+             <button onClick={handleSearch}>
+                Search
+            </button>
         </div>
     );
 }
